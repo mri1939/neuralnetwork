990669e2b3065ea771992b72f52d2eec98420dc2
kmzway87aa <mukhlasrosyadi@outlook.com>
2018-04-05 16:42:58 +0700

    Edit number of loop

--- singlelayer/main.c
+++ singlelayer/main.c
@@ -18,7 +18,7 @@ int main()
         {0.0}, 
         {1.0}};
     createNetwork(2, 1, 0.03);
-    learn(4, x, y, 50000);
+    learn(4, x, y, 500000);
     printf("1 : %f \n", predict(x[0])[0]);
     printf("2 : %f \n", predict(x[1])[0]);
     printf("3 : %f \n", predict(x[2])[0]);
